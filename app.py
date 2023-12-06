696ae9b51e44c5d9bc1b0df36727c78c3e381033
Rutvi Kanani <rutvi.kanani1008@gmail.com>
2023-12-06 11:41:39 -0500

    [added cors config

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
 from server.modules.paraphraser.paraphraser import paraphrase_text_generation
 from server.modules.summarizer.summarizer import summarize_text_generation
 from server.modules.translator.translator import translate_text_generation
 from server.modules.ai_chat.ai_chat import ai_chat_generation
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(
     paraphrase_text_generation, tags=["Pharaphrase Text"], prefix="/pharaphrase-Text-system/v1"
